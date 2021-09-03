da1610a2282024fa8a8560ac2278794bfc1657f5
ivan <ivan.nikolicamp@gmail.com>
2021-09-04 00:28:47 +0200

    Updates

---
+++ precache-manifest.791ddc4f7befe8e0cc05dd32cbafb30a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69c872233281b40f72043428c5da8cc",
+    "url": "/Unsplash-clone/index.html"
+  },
+  {
+    "revision": "9c21a8f1a0f8604e2e90",
+    "url": "/Unsplash-clone/static/css/main.8867819b.chunk.css"
+  },
+  {
+    "revision": "19ab73d12f1abaeb8965",
+    "url": "/Unsplash-clone/static/js/2.58bfd0e9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Unsplash-clone/static/js/2.58bfd0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c21a8f1a0f8604e2e90",
+    "url": "/Unsplash-clone/static/js/main.3b19e76f.chunk.js"
+  },
+  {
+    "revision": "8b9b0ceea549c741d612",
+    "url": "/Unsplash-clone/static/js/runtime-main.579f20b4.js"
+  },
+  {
+    "revision": "a88cc0fc920ca62c5c9181a2c81dc82a",
+    "url": "/Unsplash-clone/static/media/logo.a88cc0fc.PNG"
+  }
+]);
